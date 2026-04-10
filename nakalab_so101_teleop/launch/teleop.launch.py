65ee4711b6a60e67a50b17bc6ade98f97547c485
GAI-313 <nakatogawagai@gmail.com>
2026-04-10 11:49:36 +0900

    disable load robot_description

--- nakalab_so101_teleop/launch/teleop.launch.py
+++ nakalab_so101_teleop/launch/teleop.launch.py
@@ -3,7 +3,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, Command, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
@@ -16,9 +16,6 @@ def generate_launch_description():
 
 
     # default value
-    default_robot_description_path = os.path.join(
-        get_package_share_directory('nakalab_so101_description'), 'urdf', 'so101.urdf.xacro'
-    )
     default_rviz_path = os.path.join(
         get_package_share_directory('nakalab_so101_teleop'), 'rviz', 'teleop.rviz'
     )
@@ -26,7 +23,6 @@ def generate_launch_description():
 
     # launch configuration
     device = LaunchConfiguration('device')
-    camera_type = LaunchConfiguration('camera_type')
     use_rviz = LaunchConfiguration('use_rviz')
 
 
@@ -35,24 +31,12 @@ def generate_launch_description():
         'device', default_value='/dev/ttyACM0',
         description='Path to SO-101 leader arm device.'
     )
-    declare_camera_type = DeclareLaunchArgument(
-        'camera_type', default_value='',
-        description='Camera type of mouted arm camera. When value is empty, spawn the arm without camera. Support camera type value is...\n - d435'
-    )
     declare_use_rviz = DeclareLaunchArgument(
         'use_rviz', default_value='true',
         description='Show robot description by Rviz2'
     )
     ld.add_action(declare_device)
-    ld.add_action(declare_camera_type)
     ld.add_action(declare_use_rviz)
-
-
-    # make robot description
-    robot_description = Command([
-        'xacro ', default_robot_description_path, ' ',
-        'camera_type:=', camera_type, ' ',
-    ])
 
 
     # nodes
@@ -81,16 +65,6 @@ def generate_launch_description():
             PythonExpression(["'", platform.system(), "'", " != 'Linux'"])
         )
     )
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        emulate_tty=True,
-        remappings=[('joint_states', 'follower/joint_states')],
-        parameters=[
-            {'robot_description': robot_description}
-        ]
-    )
     rviz2 = Node(
         package='rviz2',
         executable='rviz2',
@@ -101,7 +75,6 @@ def generate_launch_description():
     )
     ld.add_action(leader_arm_driver_node)
     ld.add_action(leader_arm_driver_node_py)
-    ld.add_action(robot_state_publisher)
     ld.add_action(rviz2)
 
 
